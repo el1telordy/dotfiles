38222d7f285ab55c55a0a764aec0e5ea9c0ba177
EliteLordy <62451504+el1telordy@users.noreply.github.com>
2021-06-13 19:29:27 +0300

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,34 @@
+# Shortcuts
+alias cls="clear"
+
+# Git
+alias gst="git status"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias pull="git pull"
+alias push="git push"
+
+# alias gb="git branch"
+# alias gl="git log --oneline --decorate --color"
+# alias force="git push --force"
+# alias nuke="git clean -df && git reset --hard"
+# alias pop="git stash pop"
+# alias resolve="git add . && git commit --no-edit"
+# alias stash="git stash -u"
+# alias unstage="git restore --staged ."
+# alias wip="commit wip"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Disable Spotlight
+alias spotoff="sudo mdutil -a -i off"
+# Enable Spotlight
+alias spoton="sudo mdutil -a -i on"
+
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
